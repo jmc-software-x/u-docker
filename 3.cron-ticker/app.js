df6b23d1ccc4f348db1f1845ea533f0cf50743d3
jmc.software <j4mes.delez@gmail.com>
2025-07-30 12:32:48 -0500

    [ADD] :: Docker image versioning instructions

--- 3.cron-ticker/app.js
+++ 3.cron-ticker/app.js
@@ -4,7 +4,7 @@ let times = 0;
 
 cron.schedule("1-59/5 * * * * *", () => {
   times++;
-  console.log("running a task every second", times);
+  console.log("running a task every second :: ", times);
 });
 
 console.log("Initializing cron job...");
